25a7bbfd3a8210f51f1ac3ceedcdb1ffb07658ee
Tiffany Silva <ssilva1@LKCOMSSILVA01.infor.com>
2019-08-15 11:31:48 +0530

    refactored code

--- calculator.js
+++ calculator.js
@@ -1,122 +1,139 @@
+/**
+ * Calculator.js
+`* @Author: Tiffany Silva
+ * @Date: 14/08/2019
+`* @Description: This represents a simple calculator that can be used to perform simple arithmetic
+ *               calculations such as add, subtract, multiply, divide.
+ */
+class Calculator {
+ 
+  /**
+   * 'displayVal' represents the value displayed on the calculator
+   *   'firstOperand' represents the first operand of the expression
+   *     'waitingForSecondOperand' represents the state of the expression as to 
+   *            whether the calculator is waiting for the second operand to be inputed by the user
+   *    'operator' represents the operator of the expression
+   */
 
-const calculator={
-  displayVal:"0",
-  firstOperand: null,
-  waitingForSecondOperand:false,
-  operator:null,
-};
+    //constructor
+  //initializes all default values
+  constructor() {
+    this.displayVal = "0";
+    this.firstOperand = null;
+    this.waitingForSecondOperand = false;
+    this.operator = null;
+  }
 
-console.log(calculator.displayVal);
+   //called right after object instantiation to
+  //set the display value and bind click events
+  run() {
+    this.setDisplayValue();
+    this.bindClickHandlers();
+  }
 
-function updateScreen(){
-  document.getElementById("calculation").innerHTML=calculator.displayVal;
-}
-updateScreen();
-
-const keys=document.querySelectorAll(".tg td");
-if(keys){
-
-  keys.forEach(e=>e.addEventListener("click",function (event){
-    const{target}=event;
-    if(target.classList.contains("operator")){
-      //console.log("operator",target.innerHTML);
-      handleOperator(target.innerHTML);
-      
+  //detects the operands of the expression entered by the user
+  inputValue(digit) {
+    if (this.waitingForSecondOperand === true) {
+      this.displayVal = digit;
+      this.waitingForSecondOperand = false;
+    } else {
+      if (this.displayVal === "0") {
+        this.displayVal = digit;
+        return;
+      } else {
+        this.displayVal = this.displayVal + digit;
+        return;
+      }
     }
-    if(target.classList.contains("numpad")){
-      //console.log("numpad",target.innerHTML);
-      inputVal(target.innerHTML);
-     
+  }
+
+  //detects the decimal notation and appends if necessary
+  inputDec(decimal) {
+    if (this.waitingForSecondOperand === true) return;
+
+    if (!this.displayVal.includes(decimal)) {
+      this.displayVal += decimal;
     }
-    if(target.classList.contains("decimal")){
-      inputDec(target.innerHTML);
+  }
 
-    }
-    if(target.classList.contains("all-clear")){
-     // console.log("all-clear",target.innerHTML);
-      ClearScreen();
-     
-    }
-    updateScreen();
-    return;
-  }));
+  //handles the operator of the expression
+  handleOperator(nOperator) {
+    const inputVal = parseFloat(this.displayVal);
 
-  
-}
-
-function inputVal(digit){
-  const {displayVal, waitingForSecondOperand}=calculator;
-
-  if(waitingForSecondOperand===true){
-    calculator.displayVal=digit;
-    calculator.waitingForSecondOperand=false;
-  }else{
-    const { displayVal } = calculator;
-    if(displayVal==="0"){
-      calculator.displayVal=digit;
+    if (this.operator && this.waitingForSecondOperand) {
+      this.operator = nOperator;
       return;
-    }else{
-      calculator.displayVal= displayVal + digit;
-      return;
+    }
+
+    if (this.firstOperand == null) {
+      this.firstOperand = inputVal;
+    } else if (this.operator) {
+      const currentVal = this.firstOperand || 0;
+
+      const result = this.performCal[this.operator](currentVal, inputVal);
+      this.displayVal = String(result);
+      this.firstOperand = result;
+    }
+
+    this.waitingForSecondOperand = true;
+    this.operator = nOperator;
+  }
+  //performs the appropriate calculation
+  performCal = {
+    "+": (firstOperand, secondOperand) => firstOperand + secondOperand, //performs addition on two numbers
+
+    "*": (firstOperand, secondOperand) => firstOperand * secondOperand, //performs multiplication on two numbers
+
+    "/": (firstOperand, secondOperand) => firstOperand / secondOperand, //performs division on two numbers
+
+    "-": (firstOperand, secondOperand) => firstOperand - secondOperand, //performs subtraction on two numbers
+
+    "=": (firstOperand, secondOperand) => secondOperand
+  };
+
+  //clears the display to the original state
+  clearScreen() {
+    this.displayVal = "0";
+    this.firstOperand = null;
+    this.waitingForSecondOperand = false;
+    this.operator = null;
+  }
+
+  //Sets the display value of the calculator
+  setDisplayValue() {
+    let displayValue = document.getElementById("calculation");
+    displayValue.textContent = this.displayVal;
+  }
+
+  //click event that will be triggered upon oncick event
+  onClick(event) {
+    const { target } = event;
+
+    if (target.classList.contains("operator")) {
+      this.handleOperator(target.innerHTML); //calls the handleOperator method to update the operator
+    }
+    if (target.classList.contains("numpad")) {
+      this.inputValue(target.innerHTML); //calls the inputValue method to input the operands
+    }
+    if (target.classList.contains("decimal")) {
+      this.inputDec(target.innerHTML); //calls the inputDec method to input the decimal
+    }
+    if (target.classList.contains("clear-all")) {
+      this.clearScreen(); //calls the clearScreen method to clear the display
+    }
+    this.setDisplayValue(); //calls the setDisplayValue method to update the display value
+    return;
+  }
+
+  //binds click events to each table cells
+  bindClickHandlers() {
+    const keys = document.querySelectorAll(".tg td");
+    if (keys) {
+      keys.forEach(e => e.addEventListener("click", this.onClick.bind(this)));
     }
   }
 }
 
-function inputDec(decimal){
-  
-  if(calculator.waitingForSecondOperand===true) return;
-
-  if(calculator.displayVal.includes(decimal)){
-    calculator.displayVal+=decimal;
-  }
-}
-
-function handleOperator(nOperator){
-  const {firstOperand, displayVal, operator}=calculator;
-  const inputVal=parseFloat(displayVal);
-
-  if(operator && calculator.waitingForSecondOperand){
-    calculator.operator=nOperator;
-    console.log(calculator);
-    return;
-  }
-
-  if(firstOperand==null){
-    calculator.firstOperand=inputVal;
-  }else if(operator){
-    const currentVal=firstOperand||0;
-    
-    const result=performCal[operator](currentVal,inputVal);
-    calculator.displayVal=String(result);
-    calculator.firstOperand=result;
-  }
-
-  calculator.waitingForSecondOperand=true;
-  calculator.operator=nOperator;
-  console.log(calculator);
-}
-
-
-const performCal={
-  "+": (firstOperand, secondOperand)=> firstOperand + secondOperand,
-
-  "*": (firstOperand, secondOperand)=> firstOperand * secondOperand,
-  
-  "/": (firstOperand, secondOperand)=> firstOperand / secondOperand,
-  
-  "-": (firstOperand, secondOperand)=> firstOperand - secondOperand,
-  
-  "=": (firstOperand, secondOperand)=> secondOperand
-
-};
-
-function ClearScreen(){
-  calculator.displayVal="0";
-  calculator.firstOperand=null;
-  calculator.waitingForSecondOperand=false;
-  calculator.operator=null;
-  console.log(calculator);
-}
-
-
-
+//creating the object of calculator
+const calculator = new Calculator();
+calculator.run();
